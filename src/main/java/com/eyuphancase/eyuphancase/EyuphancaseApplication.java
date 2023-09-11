7f26ff91aa4eb00cd60bc4f8e218522641fc6142
Eyuphan Oguz <75530935+eyuphan-oguz@users.noreply.github.com>
2023-09-11 18:27:43 +0300

    first commit

---
+++ src/main/java/com/eyuphancase/eyuphancase/EyuphancaseApplication.java
@@ -0,0 +1,13 @@
+package com.eyuphancase.eyuphancase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyuphancaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyuphancaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyuphancase/eyuphancase/EyuphancaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyuphancase.eyuphancase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyuphancaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
